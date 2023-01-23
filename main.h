1ff67116f15f403e609e62ad686c3aab407afdc5
tomas <tomiketema209@gmail.com>
2023-01-23 04:37:02 -0800

    Fixed Team Task1

--- main.h
+++ main.h
@@ -1,55 +1,28 @@
 #ifndef MAIN_H
 #define MAIN_H
+
 #include <stdarg.h>
-#include <stdio.h>
 #include <unistd.h>
-
-#define UNUSED(x) (void)(x)
-#define BUFF_SIZE 1024
-
-/* FLAGS */
-#define F_MINUS 1
-#define F_PLUS 2
-#define F_ZERO 4
-#define F_HASH 8
-#define F_SPACE 16
-
-/* SIZES */
-#define S_LONG 2
-#define S_SHORT 1
+#include <stdlib.h>
 
 /**
- * struct fmt - Struct op
+ * struct print - struct for printer functions
+ * @type_arg: identifier
+ * @f: pointer to a printer functions
  *
- * @fmt: The format.
- * @fn: The function associated.
+ * Description: struct that stores pointers to a
+ * printer functions.
  */
-struct fmt
+typedef struct print
 {
-	char fmt;
-	int (*fn)(va_list, char[], int, int, int, int);
-};
-
-
-/**
- * typedef struct fmt fmt_t - Struct op
- *
- * @fmt: The format.
- * @fm_t: The function associated.
- */
-typedef struct fmt fmt_t;
+	char *type_arg;
+	int (*f)(va_list, char *, unsigned int);
+} print_t;
 
 int _printf(const char *format, ...);
-int handle_print(const char *fmt, int *i,
-		 va_list list, char buffer[], int flags, int width, int precision, int size);
 
-/****************** FUNCTIONS ******************/
 
-/* Funtions to print chars and strings */
-int print_char(va_list types, char buffer[],
-	       int flags, int width, int precision, int size);
-int print_string(va_list types, char buffer[],
-		 int flags, int width, int precision, int size);
-int print_percent(va_list types, char buffer[],
-		  int flags, int width, int precision, int size);
+
+
+
 #endif /* MAIN_H */
